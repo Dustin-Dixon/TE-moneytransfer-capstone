13bddb8e2ba1a5a28e90b1f03a1858e5e08f4436
Kat Kuntz <rezonantcheetah@gmail.com>
2021-03-03 11:43:00 -0500

    Add account DAO

---
+++ dotnet/TenmoServer/DAO/AccountSqlDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using TenmoServer.Models;
+
+namespace TenmoServer.DAO
+{
+    public class AccountSqlDAO : IAccountDAO
+    {
+        private readonly string connectionString;
+
+        public AccountSqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Account GetAccountByUserId(int userId)
+        {
+            Account output = null;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT account_id, user_id, balance " +
+                                   "FROM accounts " +
+                                   "WHERE user_id = @userId;";
+
+                    SqlCommand command = new SqlCommand(query, conn);
+                    command.Parameters.AddWithValue("@userId", userId);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        output = GetAccountFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return output;
+        }
+
+        private Account GetAccountFromReader(SqlDataReader reader)
+        {
+            return new Account()
+            {
+                ID = Convert.ToInt32(reader["account_id"]),
+                UserID = Convert.ToInt32(reader["user_id"]),
+                Balance = Convert.ToDecimal(reader["balance"])
+            };
+        }
+    }
+}
---
+++ dotnet/TenmoServer/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace TenmoServer.Models
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
