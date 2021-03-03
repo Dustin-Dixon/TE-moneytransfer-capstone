4e4e5469268808d06089021225cc8e20de969b04
Kat Kuntz <rezonantcheetah@gmail.com>
2021-03-03 11:43:26 -0500

    Add account endpoint

---
+++ dotnet/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountDAO accountDAO;
+
+        public AccountController(IAccountDAO accountDAO)
+        {
+            this.accountDAO = accountDAO;
+        }
+
+        [HttpGet]
+        public ActionResult GetLoggedInAccount()
+        {
+            string userIdStr = User.FindFirst("sub")?.Value;
+
+            if (userIdStr == null)
+            {
+                return Unauthorized();
+            }
+
+            int userId = Convert.ToInt32(userIdStr);
+
+            Account account = accountDAO.GetAccountByUserId(userId);
+
+            return Ok(account);
+        }
+
+    }
+}
