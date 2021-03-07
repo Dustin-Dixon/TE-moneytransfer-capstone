053c967f29f47484f38354cb2d6ac3ecbc06b57d
Kat Kuntz <rezonantcheetah@gmail.com>
2021-03-07 13:57:07 -0500

    Add model validation to transfer amount

--- dotnet/TenmoServer/Models/Transfer.cs
+++ dotnet/TenmoServer/Models/Transfer.cs
@@ -5,6 +5,11 @@ namespace TenmoServer.Models
     public abstract class TransferBase
     {
         public int TransferId { get; set; }
+
+        // A valid amount consists of at least one number followed by an optional decimal point and two numbers
+        [RegularExpression(@"^\d+(\.\d{2})?$")]
+        // The sql column is limited to 13 digits with two of them used as decimal places
+        [Range(0.01, 99999999999.99)]
         public decimal Amount { get; set; }
         public string TransferType { get; set; }
         public string TransferStatus { get; set; }
