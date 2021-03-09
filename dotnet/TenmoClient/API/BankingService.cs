41670f932175f4c5e5ef5b98644be34698e16472
Kat Kuntz <rezonantcheetah@gmail.com>
2021-03-09 12:57:56 -0500

    Clean up client error handling

--- dotnet/TenmoClient/API/BankingService.cs
+++ dotnet/TenmoClient/API/BankingService.cs
@@ -16,14 +16,9 @@ namespace TenmoClient
             RestRequest request = new RestRequest(API_BASE_URL + "account");
             IRestResponse<API_Account> response = client.Get<API_Account>(request);
 
-            if (response.ResponseStatus != ResponseStatus.Completed)
+            if (IsResponseError(response))
             {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                HandleResponseError(response, response.Data.Message);
                 return null;
             }
 
@@ -34,15 +29,10 @@ namespace TenmoClient
         {
             RestRequest request = new RestRequest(API_BASE_URL + "users");
             IRestResponse<List<UserInfo>> response = client.Get<List<UserInfo>>(request);
-
-            if (response.ResponseStatus != ResponseStatus.Completed)
+            
+            if (IsResponseError(response))
             {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                HandleResponseError(response, null);
                 return null;
             }
 
@@ -56,14 +46,9 @@ namespace TenmoClient
 
             IRestResponse<API_Transfer> response = client.Post<API_Transfer>(request);
 
-            if (response.ResponseStatus != ResponseStatus.Completed)
+            if (IsResponseError(response))
             {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                HandleResponseError(response, response.Data.Message);
                 return null;
             }
 
@@ -75,14 +60,9 @@ namespace TenmoClient
             RestRequest request = new RestRequest(API_BASE_URL + "transfers");
             IRestResponse<List<API_Transfer>> response = client.Get<List<API_Transfer>>(request);
 
-            if (response.ResponseStatus != ResponseStatus.Completed)
+            if (IsResponseError(response))
             {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                HandleResponseError(response, null);
                 return null;
             }
 
@@ -94,18 +74,34 @@ namespace TenmoClient
             RestRequest request = new RestRequest(API_BASE_URL + $"transfers/{transferId}");
             IRestResponse<API_Transfer> response = client.Get<API_Transfer>(request);
 
-            if (response.ResponseStatus != ResponseStatus.Completed)
+            if (IsResponseError(response))
             {
-                Console.WriteLine("An error occurred communicating with the server.");
-                return null;
-            }
-            else if (!response.IsSuccessful)
-            {
-                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                HandleResponseError(response, response.Data.Message);
                 return null;
             }
 
             return response.Data;
+        }
+
+        public bool IsResponseError(IRestResponse response)
+        {
+            return (response.ResponseStatus != ResponseStatus.Completed || !response.IsSuccessful);
+        }
+
+        public void HandleResponseError(IRestResponse response, string message)
+        {
+            if (response.ResponseStatus != ResponseStatus.Completed)
+            {
+                Console.WriteLine("An error occurred communicating with the server.");
+            }
+            else if (!response.IsSuccessful)
+            {
+                Console.WriteLine("An error response was received from the server. The status code is " + (int)response.StatusCode);
+                if (!string.IsNullOrEmpty(message))
+                {
+                    Console.WriteLine($"Error Message: {message}");
+                }
+            }
         }
 
         public void Login(API_User user)
